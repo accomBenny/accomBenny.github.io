a356fe9e5dfd742f74e3fede9c19e8afeba4f1af
accomBenny <cs921029@github.com>
2023-11-05 21:20:45 +0800

    remove console log

---
+++ main.js
@@ -0,0 +1,12 @@
+$(document).ready(function () {
+    
+    $("#upload").click(function () { 
+        const text = $("#textContent").text();  
+        $("#copyText").text(text);
+        $(".copyMessage").animate({
+            
+        })
+
+    });
+    
+});
